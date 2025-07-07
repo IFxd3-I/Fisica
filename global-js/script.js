69ca31071adb042a58c97588d1a4bb3f1587149c
IFxd3-I <215060833+IFxd3-I@users.noreply.github.com>
2025-07-07 14:33:26 +0200

    fix ai link e aggiunta file

---
+++ global-js/script.js
@@ -0,0 +1,17 @@
+function openLightbox(imgElement) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = imgElement.src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
+
+document.querySelectorAll('ex .procedimento').forEach(procedimento => {
+    procedimento.addEventListener('click', () => {
+        procedimento.classList.toggle('unblurred');
+    });
+});
