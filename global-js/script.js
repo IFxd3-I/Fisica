b562549dca6f6178106c894134bbef8a052224cf
IFxd3-I <215060833+IFxd3-I@users.noreply.github.com>
2025-09-30 13:29:22 +0200

    ultimo push prima del cambio

--- global-js/script.js
+++ global-js/script.js
@@ -19,27 +19,4 @@ document.addEventListener('DOMContentLoaded', function () {
             procedimento.addEventListener('click', () => procedimento.classList.toggle('unblurred'));
         });
     }
-
-    const yearBoxes = document.querySelectorAll('.year-box');
-    const initialView = document.querySelector('h1');
-    const yearsContainer = document.querySelector('.years-container');
-    const yearSections = document.querySelectorAll('.year-section');
-    const backBtns = document.querySelectorAll('.back-btn');
-
-    yearBoxes.forEach(box => {
-        box.addEventListener('click', function() {
-            const year = this.dataset.year;
-            initialView.style.display = 'none';
-            yearsContainer.style.display = 'none';
-            document.getElementById(`year-${year}`).style.display = 'block';
-        });
-    });
-
-    backBtns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            yearSections.forEach(section => section.style.display = 'none');
-            initialView.style.display = 'block';
-            yearsContainer.style.display = 'flex';
-        });
-    });
 });
